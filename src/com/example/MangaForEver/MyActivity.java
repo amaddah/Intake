85c3322c71eeedd175fbebba63cdf3d6a423db2b
amaddah <amine.maddah@insa-cvl.fr>
2015-11-15 10:42:33 +0100

    Ajout image de starting + touch listener

--- src/com/example/MangaForEver/MyActivity.java
+++ src/com/example/MangaForEver/MyActivity.java
@@ -1,32 +1,40 @@
 package com.example.MangaForEver;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.view.TextureView;
+import android.os.Handler;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.*;
-import org.w3c.dom.Text;
 
-public class MyActivity extends Activity implements View.OnClickListener {
+public class MyActivity extends Activity implements View.OnClickListener, View.OnTouchListener {
     /**
      * Called when the activity is first created.
      */
-    private Button boutonMain;
-    private Button boutonEntrer;
+    private Button
+                    boutonMain,
+                    boutonEntrer;
     private EditText t;
     private TextView s;
     private String prenom;
     private CheckBox plaitIl;
+    private LinearLayout l1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.firstimage);
+        l1 = (LinearLayout) findViewById(R.id.l1);
+        l1.setOnTouchListener(this);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
         setContentView(R.layout.main);
-
         boutonMain = (Button) findViewById(R.id.boutonMain);
-
         boutonMain.setOnClickListener(this);
+        return false;
     }
 
     @Override
@@ -36,23 +44,16 @@ public class MyActivity extends Activity implements View.OnClickListener {
             case R.id.boutonMain:
                 t = (EditText) findViewById(R.id.name);
                 prenom = t.getText().toString();
-
                 setContentView(R.layout.presentation);
                 s = (TextView) findViewById(R.id.prenomAffiche);
                 s.setText("Bonjour " + prenom + " !");
                 boutonEntrer = (Button) findViewById(R.id.jentre);
-                boutonEntrer.setOnClickListener(this);
+                boutonEntrer.setOnClickListener(this); // Bouton "j'entre" guidant vers l'application en-elle même.
                 break;
             case R.id.jentre:
                 plaitIl = (CheckBox) findViewById(R.id.plaitIl);
-                if ( plaitIl.isChecked() ) {
-                    // C'est bon
-                    setContentView(R.layout.finintro);
-                }
-                else {
-                    // Pas bon
-                    Toast.makeText(MyActivity.this, "Oh oh, tu n'as pas coché la case \"Ce projet me plait\".", Toast.LENGTH_SHORT).show();
-                }
+                if ( plaitIl.isChecked() ) setContentView(R.layout.finintro);// C'est bon
+                else Toast.makeText(MyActivity.this, R.string.ideeprojet, Toast.LENGTH_SHORT).show();// Pas bon
                 break;
         }
     }
